15ec36a1816a2520c40f9e56b6a3681a9f5dfc1c
Marcus000461 <mv2882106@gmail.com>
2026-02-01 17:44:53 -0300

    Change salary adjustment from discount to increase

--- Reajuste salarial.py
+++ Reajuste salarial.py
@@ -1,3 +1,3 @@
 salário = float(input('Qual é o salário do Funcionario? R$'))
-novo_salário = salário - (salário * 15/100)
-print('O funcionario que antes ganhava R${:.2f}, agora com 15% de desconto ganha R${:.2f}'.format(salário,novo_salário))
+novo_salário = salário + (salário * 15/100)
+print('O funcionario que antes ganhava R${:.2f}, agora com 15% de aumento ganha R${:.2f}'.format(salário,novo_salário))
